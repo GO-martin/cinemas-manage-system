66d3d191711569c3ef8e433ef78e1ea1c9951ca4
GO-martin <martin.nguyen.goldenowl@gmail.com>
2023-11-28 15:51:52 +0700

    fix service and add rooms_helper

--- app/helpers/customer/rooms_helper.rb
+++ app/helpers/customer/rooms_helper.rb
@@ -1,2 +1,13 @@
 module Customer::RoomsHelper
+  def get_structure(row_index, column_index)
+    @structures.where(row_index:, column_index:).first
+  end
+
+  def get_sold_ticket(row_index, column_index)
+    @sold_tickets.where(seat_row: row_index, seat_column: column_index)
+  end
+
+  def get_selecting_seat(row_index, column_index)
+    @selecting_seats.where(row_index:, column_index:)
+  end
 end
