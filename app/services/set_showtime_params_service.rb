979cca390fb4f8e00ba5d2c38a1b2ed391968538
GO-martin <martin.nguyen.goldenowl@gmail.com>
2023-11-28 14:12:48 +0700

    Add set showtime params service

---
+++ app/services/set_showtime_params_service.rb
@@ -0,0 +1,17 @@
+class SetShowtimeParamsService < ApplicationService
+  attr_accessor :params
+
+  def initialize(params)
+    super
+    @params = params
+  end
+
+  def call
+    params[:duration] = Movie.find_by(id: params[:movie_id]).length
+    parsed_start_time = Time.parse(params[:start_time])
+    params[:end_time] = parsed_start_time + params[:duration].minutes
+    params[:cinema_id] = Room.find_by(id: params[:room_id]).cinema.id
+    params[:location_id] = Room.find_by(id: params[:room_id]).cinema.location.id
+    params
+  end
+end
