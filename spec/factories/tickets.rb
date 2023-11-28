3a6a28c92e22ce1b5af0787b091769678714404b
GO-martin <martin.nguyen.goldenowl@gmail.com>
2023-11-28 11:01:34 +0700

    Add constant LETTERS

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,5 +1,5 @@
 def seat_number_format(row_index, column_index)
-  letters = ('A'..'Z').to_a
+  letters = ApplicationHelper::LETTERS
   letter = letters[row_index]
   "#{letter}#{column_index}"
 end
